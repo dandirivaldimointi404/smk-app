bb0fc90a8c7a5ef2859accda0ea6a17bf9357c32
DandiRivaldi404 <yukngoding404@gmail.com>
2023-11-30 00:39:58 +0800

    api login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -12,15 +12,21 @@ class AuthController extends Controller
     public function login(Request $request)
     {
         if (!Auth::attempt($request->only('username', 'password'))) {
-            return response()
-                ->json(['message' => 'Unauthorized'], 401);
+            return response()->json(['message' => 'Unauthorized'], 401);
         }
 
-        $user = User::where('username', $request[''])->firstOrFail();
+        $user = User::where('username', $request['username'])->firstOrFail();
+
+        if ($user->level !== 'siswa') {
+            return response()->json(['message' => 'Unauthorized'], 401);
+        }
 
         $token = $user->createToken('auth_token')->plainTextToken;
 
-        return response()
-            ->json(['message' => 'Hi ' . $user->name . ', welcome to home', 'access_token' => $token, 'token_type' => 'Bearer',]);
+        return response()->json([
+            'message' => 'Hi ' . $user->name . ', welcome to home',
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
     }
 }
