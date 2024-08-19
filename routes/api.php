3e1be7eef793825fcf4a3d4faba0537621d7d44d
dandirivaldimointi404 <152193667+dandirivaldimointi404@users.noreply.github.com>
2024-08-19 16:22:30 +0800

    testint

--- routes/api.php
+++ routes/api.php
@@ -21,7 +21,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::middleware(['guest'])->group(function () {
-    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('login', [AuthController::class, 'login']);
 });
 
 
